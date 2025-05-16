9036953a3b2de278e001657e8c41abb718a25115
Diego Coelho <diegocoelhoaprova@gmail.com>
2025-05-15 23:20:29 -0300

    Bloqueando horários já agendados

--- src/modules/schedules/load.js
+++ src/modules/schedules/load.js
@@ -17,6 +17,6 @@ export async function schedulesDay() {
   schedulesShow({ dailySchedules })
 
   // Renderiza as horas disponíveis
-  hoursLoad({ date })
+  hoursLoad({ date, dailySchedules })
 
 }
