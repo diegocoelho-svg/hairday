84047a8afb44197ee41472e3fac5265b5168af7a
Diego Coelho <diegocoelhoaprova@gmail.com>
2025-05-15 22:15:36 -0300

    Registrando um novo agendamento / Serviço para buscar agendamento do dia

--- src/services/schedule-new.js
+++ src/services/schedule-new.js
@@ -1,19 +1,18 @@
-import { apiConfig } from "./api-config.js";
+import { apiConfig } from "./api-config.js"
 
 export async function scheduleNew({ id, name, when }) {
+
   try { 
+    // Faz a requisição para enviar os dados de agendamento.
     await fetch(`${apiConfig.baseURL}/schedules`, {
-      method: 'POST',
+      method: "POST",
       headers: {
         "Content-Type": "application/json"
       },
-      body: JSON.stringify({
-        id,
-        name,
-        when
-      }),
+      body: JSON.stringify({ id, name, when }),
     })
-    
+
+    // Exibe mensagem de agendamento realizado.
     alert("Agendamento confirmado com sucesso!")
   } catch (error) {
     console.log(error)
